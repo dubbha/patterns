bc384a1e601d5a4d5a21dd93b6d5488ce39b6ae6
Oleksiy Dubovyk <oleksiy_dubovyk@epam.com>
2018-06-17 16:37:20 +0300

    state

---
+++ state.md
@@ -0,0 +1,7 @@
+# State
+
+### UML diagram
+![state](img/state.svg)
+
+### Code
+ - [TypeScript](state.ts)
---
+++ state.ts
@@ -0,0 +1,39 @@
+namespace StatePattern {
+  class MobileAlertContext {                   // Context
+    state: MobileAlertState
+
+    setState(state: MobileAlertState) {
+      this.state = state;
+    }
+
+    request() {
+      this.state.handle();
+    }
+  }
+
+  abstract class MobileAlertState {            // State
+    abstract handle();
+  }
+
+  class Vibration extends MobileAlertState {
+    handle() {
+      console.log('vibrating...');
+    }
+  }
+
+  class Silent extends MobileAlertState {
+    handle() {
+      console.log('silent...');
+    }
+  }
+
+  // Client code
+  const context = new MobileAlertContext();
+  context.setState(new Vibration());
+  context.request();                    // vibrating...
+  context.request();                    // vibrating...
+  context.setState(new Silent());
+  context.request();                    // silent...
+  context.request();                    // silent...
+
+}
